9b93c1e60424511ced83424e10d97448301a45dc
wjy <wjy@qq.com>
2021-05-18 20:28:55 +0800

    增加双缓冲电路

--- design/test_git.v
+++ design/test_git.v
@@ -6,10 +6,21 @@ module	test_git(
 );
 
 reg	d;
+reg rst1,rst2;
 
-always@(posedge clk	or negedge rst)
+//双缓冲电路，去除亚稳态
+always@(posedge clk or negedge rst)
 begin
-		if(!rst)
+	if(!rst)
+		begin
+			{rst2,rst1}<=2'b11;
+		end
+		  {rst2,rst1}<={rst1,rst};
+end
+
+always@(posedge clk	or negedge rst2)
+begin
+		if(!rst2)
 			d<=0;
 		else
 			d<=~data;
